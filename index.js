4d4516fcd98165e23acc8048be77e8894e219bfe
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-08-28 19:58:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function driversByName(drivers) {
 function totalRevenue(drivers) {
  const totalRevDrivers = [...drivers]
   totalRevDrivers.reduce(function (agg, el, i, arr) {
-    return agg.revenue
+    return agg.totalRev
   }, totalRev: 0)
   
 }
